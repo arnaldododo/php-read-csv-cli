ede2e1e54c0e459a7386fd9f54bdbac324026528
Muhammad Arnaldo <muhammad.arnaldo@gmail.com>
2025-02-16 22:27:33 +0700

    feat/MDL-2 add preprocessing to the incoming data

--- FileHandler.php
+++ FileHandler.php
@@ -28,13 +28,70 @@ class FileHandler
 
         $data = [];
         if (($handle = fopen($this->filePath, "r")) !== false) {
+            fgetcsv($handle); // Skip the header row
             while (($row = fgetcsv($handle, 1000, ",")) !== false) {
                 $data[] = $row;
             }
             fclose($handle);
         }
 
-        return $data;
+        return $this->preprocessData($data);
+    }
+
+    /**
+     * Preprocesses the data.
+     *
+     * @param array $row The row to be preprocessed.
+     * @return array The preprocessed row.
+     */
+    private function preprocessData(array $data)
+    {
+        $preprocessedData = [];
+        foreach ($data as $row) {
+            $row[0] = $this->cleanName($row[0]);
+            $row[1] = $this->cleanName($row[1]);
+            $temp = $row[2];
+            if (!$row[2] = $this->cleanEmail($row[2])) {
+                echo $temp . " is not a valid email address. Skipping this entry.\n\n";
+                continue;
+            }
+            $preprocessedData[] = $row;
+        }
+        return $preprocessedData;
+    }
+
+    /**
+     * Cleans the name column.
+     *
+     * @param string $name
+     * @return string The cleaned name.
+     */
+    private function cleanName(string $name)
+    {
+        $name = trim($name);
+        $name = strtolower($name);
+        $name = ucwords($name);
+        $name = preg_replace("/[^\p{L}']/u", "", $name);
+
+        return $name;
+    }
+
+    /**
+     * Cleans and validates an email address.
+     *
+     * @param string $email The raw email address.
+     * @return string The cleaned email if valid, otherwise false.
+     */
+    private function cleanEmail(string $email)
+    {
+        $email = trim($email);
+        $email = strtolower($email);
+
+        $domain = substr(strrchr($email, "@"), 1);
+        if (!preg_match('/^[\w.-]+@[\w.-]+\.[a-z]{2,}$/', $email) || !checkdnsrr($domain)) {
+            return false;
+        }
+        return $email;
     }
 
     /**
